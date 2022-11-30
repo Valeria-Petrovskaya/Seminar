6874db0fef3918d623a17af7aea6a04e2fbc7dbd
Lera <miss_val@bk.ru>
2022-11-30 12:16:16 +0300

    Задача 15

---
+++ 3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("День недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Такого дня нет, введите заново: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n ==6 || n==7)
+    Console.Write("Выходной день");
+else
+    Console.Write("Будний день");
